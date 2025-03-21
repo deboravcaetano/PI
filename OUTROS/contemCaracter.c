571a016ab6d6a005836633a4c6738ccd1071b50c
a112332 <a112332@alunos.uminho.pt>
2025-03-21 14:47:53 +0000

    alteração ex verifica caracter

--- OUTROS/contemCaracter.c
+++ OUTROS/contemCaracter.c
@@ -2,14 +2,23 @@
  
 int contemChar(char s[],char c){
     int i;
-    int encontrado = 0;
-    for(i=0; s[i] != '\0' && !(encontrado==1);i++){
+    int encontrado = 0; // usamos uma flag para determinar quando encerrar o loop for
+    for(i=0; s[i] != '\0' && !(encontrado==1);i++){ 
         if(s[i]==c){
             encontrado=1;
         }
     }
     return encontrado;
 }
+
+/*
+O objetivo é que não faça comparações desnecessárias, ou seja, se já encontrou o elemento,
+não precisa de procurar a sua existência nos próximos índices.
+
+O loop for deve parar quando for encontrado o elemento, ou seja quando encontrado == 1, ele realiza o ciclo for enquanto
+não encontrar -> !(encontrado==1).
+
+*/
 
 
 int main(){
